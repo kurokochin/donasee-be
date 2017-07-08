7e5f4527e8631a480ddc09fa319dc3c149f53b21
ricky <rickyputra98@gmail.com>
2017-07-09 01:25:06 +0700

    allow any campaign

--- donasee/api/views/campaign.py
+++ donasee/api/views/campaign.py
@@ -1,4 +1,5 @@
 from rest_framework import status
+from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
@@ -7,6 +8,8 @@ from donasee.apps.campaign.models import Campaign
 
 
 class CampaignListView(APIView):
+    permission_classes = AllowAny
+
     def get(self, request, format=None):
         return Response(CampaignSerializer(Campaign.objects.all(), many=True).data)
 
