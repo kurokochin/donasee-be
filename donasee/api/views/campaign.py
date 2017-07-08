784c85608dcc69430dc29ebf3adac9d96b8a85b2
ricky <rickyputra98@gmail.com>
2017-07-09 02:09:38 +0700

    add community name in campaign

--- donasee/api/views/campaign.py
+++ donasee/api/views/campaign.py
@@ -1,9 +1,12 @@
+from django.contrib.auth.models import User
 from rest_framework import status
 from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
 from donasee.api.serializers.campaign import CampaignSerializer
+from donasee.api.serializers.user import UserProfileSerializer
+from donasee.apps.accounts.models import UserProfile
 from donasee.apps.campaign.models import Campaign
 
 
@@ -11,12 +14,19 @@ class CampaignListView(APIView):
     permission_classes = (AllowAny,)
 
     def get(self, request, format=None):
-        return Response(CampaignSerializer(Campaign.objects.all(), many=True).data)
+        campaigns = CampaignSerializer(Campaign.objects.all(), many=True).data
+        datas = []
+        for campaign in campaigns:
+            user_profile = UserProfileSerializer(
+                instance=UserProfile.objects.get(user=User.objects.get(id=campaign['user']))).data
+            data = campaign
+            data['community_name'] = user_profile['community_name']
+            datas.append(data)
+        return Response(datas)
 
     def post(self, request, format=None):
         ser = CampaignSerializer(data=request.data)
         if ser.is_valid():
             ser.save()
             return Response(ser.data)
-        print(ser.errors)
         return Response({'detail': ser.errors['non_field_errors'][0]}, status=status.HTTP_400_BAD_REQUEST)
